4621f47857b7c09341ba82f8a021def0fea0b34e
Yeguaguayu-s <yeguaguayu@gmail.com>
2026-04-28 18:40:02 +0800

    refactor: Remove unused utility scripts
    Removes several unused Node.js scripts that were previously used for fetching data from external sources. These scripts are no longer necessary for the current application functionality.

--- test_proxy.cjs
+++
@@ -1,5 +0,0 @@
-const https = require('https');
-
-https.get('https://image.baidu.com/search/down?url=https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Ma_Long_ATTC2017_29.jpeg/960px-Ma_Long_ATTC2017_29.jpeg', (res) => {
-  console.log('Headers:', res.headers);
-});
